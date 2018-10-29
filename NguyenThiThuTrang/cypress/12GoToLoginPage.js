6f97c59cb8898a1ffb24b1578756659bb257966e
trangkum <trangkum197@gmail.com>
2018-10-29 22:11:16 +0700

    upload cypress exercise

--- NguyenThiThuTrang/cypress/12GoToLoginPage.js
+++ NguyenThiThuTrang/cypress/12GoToLoginPage.js
@@ -3,8 +3,11 @@ context('Actions', ()) => {
         cy.visit('https://hoclieu.sachmem.vn/folder/0')
     })
 
-    it('go to login page', () => {
-        cy.get('a[id="link-login"]').click()
-        cy.url().should('eq', 'https://accounts.sachmem.vn/users/sign_in')
+    describe('Test go to login page', () => {
+        it('go to login page', () => {
+            cy.get('a[id="link-login"]').click()
+            cy.url().should('eq', 'https://accounts.sachmem.vn/users/sign_in')
+        })
     })
+
 }
